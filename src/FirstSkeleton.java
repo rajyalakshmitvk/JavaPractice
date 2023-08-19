67d5bb906e55ca03e55ef440f4421da93cf3b183
rajyalakshmitvk <rajyalakshmitvk@gmail.com>
2023-08-19 12:53:30 +0530

    Skeleton of Java program

---
+++ src/FirstSkeleton.java
@@ -0,0 +1,28 @@
+/*
+Documentation Section
+Package section
+Class Definition
+class classname
+{
+}
+Main Class Definition
+public class MainClassName
+{
+    //Main Method Definition
+    public static void main(String args[])
+    {
+        //Main Method body
+    }
+}
+*/
+/*
+Java Program Structure, Author: M. RajyaLakshmi, Date: 19-08-2023
+*/
+//import java.lang.*; //Import section
+//Class Definition section
+public class FirstSkeleton { // Main class definition
+    static public void main(String args[]) //Main method definition
+    {
+        System.out.println("Welcome to Java program");
+    }
+}
